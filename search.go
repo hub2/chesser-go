c53c65cf05bce2946e96aa7ebb144dd68fcf7b33
Hubert Barc <barchubert@gmail.com>
2019-01-18 00:53:06 +0100

    3fold fix?

--- search.go
+++ search.go
@@ -128,7 +128,17 @@ func negaMax(board *dt.Board, depth int, alpha, beta int, moveList []dt.Move, do
 	if !searching {
 		return -evalBoard(board, nil), 0
 	}
-
+	if board.Halfmoveclock >= 100 {
+		return 0, 0
+	}
+	if board.Halfmoveclock > 1 {
+		// Check for 3fold
+		for i := 0; i < 4; i++ {
+			if board.Last4Hashes[i] == board.Hash() {
+				return 0, 0
+			}
+		}
+	}
 	if depth == 0 || len(moveList) == 0 {
 		val, move := quiescenceSearch(board, alpha, beta, depth)
 		return val, move
@@ -215,17 +225,6 @@ func negaMax(board *dt.Board, depth int, alpha, beta int, moveList []dt.Move, do
 func quiescenceSearch(board *dt.Board, alpha, beta, depth int) (int, dt.Move) {
 	var val int
 	var bestMove dt.Move
-	if board.Halfmoveclock >= 100 {
-		return 0, 0
-	}
-	if board.Halfmoveclock > 1 {
-		// Check for 3fold
-		for i := 0; i < 4; i++ {
-			if board.Last4Hashes[i] == board.Hash() {
-				return 0, 0
-			}
-		}
-	}
 
 	updateTimer()
 	if !searching {
